35236179157781df16caa842eba73dbe4f5cbf6e
TimurSaidov <tim77777@inbox.ru>
2019-03-02 18:31:47 +0300

    Initial Commit

---
+++ CollectionViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewApp
+//
+//  Created by Timur Saidov on 02/03/2019.
+//  Copyright © 2019 Timur Saidov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
